104cdec170d181e147b2ca30f1904beb479f6dcf
paulgb <paulgb@gmail.com>
2021-02-17 01:28:42 +0000

    Deploying to gh-pages from @ aper-dev/aper@83ae5d094ca0721d16b091c6d7ae2f7636a57406 🚀

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["aper"] = {"name":"","files":["lib.rs","state_machine.rs","suspended_event.rs","transition_event.rs"]};
+sourcesIndex["aper"] = {"name":"","dirs":[{"name":"data_structures","files":["atom.rs","mod.rs"]}],"files":["lib.rs","state_machine.rs","suspended_event.rs","transition_event.rs"]};
 sourcesIndex["aper_actix"] = {"name":"","files":["channel_actor.rs","lib.rs","messages.rs","player_actor.rs","server_actor.rs","suspended_event.rs"]};
 sourcesIndex["aper_yew"] = {"name":"","files":["lib.rs","state_manager.rs","update_interval.rs","wire_wrapped.rs"]};
 createSourceSidebar();
