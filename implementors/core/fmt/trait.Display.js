d8de8aa851a8b2f38743c38450a2a03865000b6c
paulgb <paulgb@gmail.com>
2021-02-27 06:08:21 +0000

    Deploying to gh-pages from @ aper-dev/aper@7f860712e726294ff07c6ccc6067f5c6d05b2749 🚀

--- implementors/core/fmt/trait.Display.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["aper"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"aper/struct.PlayerID.html\" title=\"struct aper::PlayerID\">PlayerID</a>","synthetic":false,"types":["aper::PlayerID"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
