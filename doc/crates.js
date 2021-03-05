5cbe26ce983e6b84aa6bad2d83b2faeb1ce7f742
paulgb <paulgb@gmail.com>
2021-03-05 01:43:55 +0000

    Deploying to gh-pages from @ aper-dev/aper@2b7e1b03d94422e0c1f438fb346e40831f4ac6ed 🚀

---
+++ doc/crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["aper","aper_actix","aper_yew"];
--- doc/implementors/aper/trait.StateProgram.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["aper"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
