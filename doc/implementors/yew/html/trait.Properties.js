67b300ea230e3ea11b832469870067984ca41688
paulgb <paulgb@gmail.com>
2021-03-13 13:07:29 +0000

    Deploying to gh-pages from @ aper-dev/aper@2335716a436f96a64505bcd86396cc2f21e5a9b5 🚀

--- doc/implementors/yew/html/trait.Properties.js
+++ doc/implementors/yew/html/trait.Properties.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["aper_yew"] = [{"text":"impl&lt;View:&nbsp;<a class=\"trait\" href=\"aper_yew/trait.StateView.html\" title=\"trait aper_yew::StateView\">StateView</a>&gt; Properties for <a class=\"struct\" href=\"aper_yew/struct.Props.html\" title=\"struct aper_yew::Props\">Props</a>&lt;View&gt;","synthetic":false,"types":["aper_yew::Props"]}];
+implementors["aper_yew"] = [{"text":"impl&lt;V:&nbsp;<a class=\"trait\" href=\"aper_yew/trait.View.html\" title=\"trait aper_yew::View\">View</a>&gt; Properties for <a class=\"struct\" href=\"aper_yew/struct.Props.html\" title=\"struct aper_yew::Props\">Props</a>&lt;V&gt;","synthetic":false,"types":["aper_yew::Props"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
