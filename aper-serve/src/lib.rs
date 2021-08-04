6fe7d354a31a3c2c7da8b5b1e34aa38f7448ee1f
Paul Butler <paulgb@gmail.com>
2021-08-04 13:34:26 +0000

    update jamsocket version and update server code

--- aper-serve/src/lib.rs
+++ aper-serve/src/lib.rs
@@ -1,9 +1,7 @@
-use std::time::Duration;
-
 use aper::StateProgram;
-use aper_jamsocket::{AperJamsocketServiceBuilder};
+use aper_jamsocket::AperJamsocketServiceBuilder;
 use env_logger::Builder;
-use jamsocket_server::{do_serve, ServerSettings, ServiceActorContext};
+use jamsocket_server::{Server, ServiceActorContext};
 
 pub fn serve<F: StateProgram>() -> std::io::Result<()> {
     let mut builder = Builder::new();
@@ -11,15 +9,10 @@ pub fn serve<F: StateProgram>() -> std::io::Result<()> {
     builder.filter(Some("jamsocket_wasm_host"), log::LevelFilter::Info);
     builder.init();
 
-    let host_factory: AperJamsocketServiceBuilder<F, ServiceActorContext> = AperJamsocketServiceBuilder::default();
+    let host_factory: AperJamsocketServiceBuilder<F, ServiceActorContext> =
+        AperJamsocketServiceBuilder::default();
 
-    let server_settings = ServerSettings {
-        heartbeat_interval: Duration::from_secs(30),
-        heartbeat_timeout: Duration::from_secs(120),
-        port: 8080,
-        room_id_strategy: Default::default(),
-        shutdown_policy: jamsocket_server::ServiceShutdownPolicy::Never,
-    };
+    let server = Server::new().with_shutdown_policy(jamsocket_server::ServiceShutdownPolicy::Never);
 
-    do_serve(host_factory, server_settings)
+    server.serve(host_factory)
 }
