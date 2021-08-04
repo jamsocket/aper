2c3211803d5dbbd76136960105e3abadbb3bb502
Paul Butler <paulgb@gmail.com>
2021-08-04 00:50:23 +0000

    omitted changes from wasi example

--- examples/aper-wasm/build.sh
+++ examples/aper-wasm/build.sh
@@ -4,9 +4,8 @@ set -e
 
 rm -f out.wasm out.wat
 
-cargo build --release --target wasm32-unknown-unknown
+cargo build --release --target wasm32-wasi
 
-wasm-opt -O2 target/wasm32-unknown-unknown/release/aper_wasm.wasm -o out.wasm
+wasm-opt -O2 target/wasm32-wasi/release/aper_wasm.wasm -o out.wasm
 
 wasm2wat out.wasm -o out.wat
-
