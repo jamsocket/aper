07ed40aad85e1d161a97dcbbaa567baab5713e2c
Paul Butler <paul@driftingin.space>
2024-10-05 08:52:05 -0700

    nit per coderabbit

--- aper-leptos/src/lib.rs
+++ aper-leptos/src/lib.rs
@@ -1,18 +1,18 @@
 use aper::{data_structures::Atom, AperSync};
-use leptos::{create_signal, SignalGet, SignalSet};
+use leptos::{create_signal, ReadSignal, SignalSet};
 use serde::{de::DeserializeOwned, Serialize};
 
 pub mod init_tracing;
 
 pub trait Watch<T> {
-    fn watch(&self) -> Box<dyn Fn() -> T>;
+    fn watch(&self) -> ReadSignal<T>;
 }
 
 impl<T> Watch<T> for Atom<T>
 where
     T: Serialize + DeserializeOwned + Default + Clone + Send + Sync + 'static,
 {
-    fn watch(&self) -> Box<dyn Fn() -> T> {
+    fn watch(&self) -> ReadSignal<T> {
         let (signal, set_signal) = create_signal(self.get());
 
         let self_clone = self.clone();
@@ -21,6 +21,6 @@ where
             true
         });
 
-        Box::new(move || signal.get())
+        signal
     }
 }
